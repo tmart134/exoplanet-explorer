eee79c1b2ba2b2f409abe910e81b72abfa1be0a3
Tracey Martin <traceymartin@STELLA0071.home>
2018-10-26 18:55:00 -0400

    updated file

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -59,6 +59,14 @@ Instructions:
     Your code goes here! Uncomment the next line when you're ready to start!
      */
 
-    // getJSON('../data/earth-like-results.json')
+    getJSON('../data/earth-like-results.json')
+    .then(function(response){
+
+      addSearchHeader(response.query);
+      response.results.map(function(url){
+        getJSON(url).then(createPlanetThumb);
+      });
+
+    });
   });
 })(document);
